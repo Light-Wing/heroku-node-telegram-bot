f67a7fda8ee59648421f14aacccbe7a31e3e2f7a
Light <manknapp@gmail.com>
2020-11-08 21:28:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,6 @@ bot.getMe().then(function (me) { //self check
   return botName;
 })
 
-bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text));
+bot.on('text', msg => bot.sendMessage(msg.from.id, msg.text + "test nodejs bot"));
 
 bot.start();
